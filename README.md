eb1ea8717e295eac1494b7c029400b2f878bd2f6
Ben Waters <ben@book-md.com>
2016-05-02 16:51:26 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wait-for-it-dns
+
+Based off of (https://github.com/vishnubob/wait-for-it)[https://github.com/vishnubob/wait-for-it], but instead attempts to do a host lookup with the built in dns settings of wherever the script is being run. Same usage, but instead of a host:port, make sure to provide a dns name instead.
---
+++ wait-for-it-dns.sh
@@ -0,0 +1,153 @@
+#!/usr/bin/env bash
+#   Use this script to test if a given record exists 
+
+cmdname=$(basename $0)
+
+echoerr() { if [[ $QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
+
+usage()
+{
+    cat << USAGE >&2
+Usage:
+    $cmdname dns-name [-s] [-t timeout] [-- command args]
+    -s | --strict               Only execute subcommand if the test succeeds
+    -q | --quiet                Don't output any status messages
+    -t TIMEOUT | --timeout=TIMEOUT
+                                Timeout in seconds, zero for no timeout
+    -- COMMAND ARGS             Execute command with args after the test finishes
+USAGE
+    exit 1
+}
+
+timeout() {
+    time=$1
+    shift
+
+    # start the command in a subshell to avoid problem with pipes
+    # (spawn accepts one command)
+    command="/bin/sh -c \"$@\""
+
+    expect -c "set echo \"-noecho\"; set timeout $time; spawn -noecho $command; expect timeout { exit 1 } eof { exit 0 }"    
+
+    if [ $? = 1 ] ; then
+       return 1 
+    fi
+
+}
+
+wait_for()
+{
+    if [[ $TIMEOUT -gt 0 ]]; then
+        echoerr "$cmdname: waiting $TIMEOUT seconds for $DNS_NAME"
+    else
+        echoerr "$cmdname: waiting for $DNS_NAME without a timeout"
+    fi
+    start_ts=$(date +%s)
+    while :
+    do
+        (host $DNS_NAME) >/dev/null 2>&1
+        result=$?
+        if [[ $result -eq 0 ]]; then
+            end_ts=$(date +%s)
+            echoerr "$cmdname: $DNS_NAME is available after $((end_ts - start_ts)) seconds"
+            break
+        fi
+        sleep 1
+    done
+    return $result
+}
+
+wait_for_wrapper()
+{
+    # In order to support SIGINT during timeout: http://unix.stackexchange.com/a/57692
+    if [[ $QUIET -eq 1 ]]; then
+        timeout $TIMEOUT $0 $DNS_NAME --quiet --child --timeout=$TIMEOUT &
+    else
+        timeout $TIMEOUT $0 $DNS_NAME --child --timeout=$TIMEOUT &
+    fi
+    PID=$!
+    trap "kill -INT -$PID" INT
+    wait $PID
+    RESULT=$?
+    if [[ $RESULT -ne 0 ]]; then
+        echoerr "$cmdname: timeout occurred after waiting $TIMEOUT seconds for $DNS_NAME"
+    fi
+    return $RESULT
+}
+
+# process arguments
+DNS_NAME=$1
+shift 1
+while [[ $# -gt 0 ]]
+do
+    case "$1" in
+        --child)
+        CHILD=1
+        shift 1
+        ;;
+        -q | --quiet)
+        QUIET=1
+        shift 1
+        ;;
+        -s | --strict)
+        STRICT=1
+        shift 1
+        ;;
+        -t)
+        TIMEOUT="$2"
+        if [[ $TIMEOUT == "" ]]; then break; fi
+        shift 2
+        ;;
+        --timeout=*)
+        TIMEOUT="${1#*=}"
+        shift 1
+        ;;
+        --)
+        shift
+        CLI="$@"
+        break
+        ;;
+        --help)
+        usage
+        ;;
+        *)
+        echoerr "Unknown argument: $1"
+        usage
+        ;;
+    esac
+done
+
+
+if [[ "$DNS_NAME" == "" ]]; then
+    echoerr "Error: you need to provide a dns name to test."
+    usage
+fi
+
+TIMEOUT=${TIMEOUT:-15}
+STRICT=${STRICT:-0}
+CHILD=${CHILD:-0}
+QUIET=${QUIET:-0}
+
+if [[ $CHILD -gt 0 ]]; then
+    wait_for
+    RESULT=$?
+    exit $RESULT
+else
+    if [[ $TIMEOUT -gt 0 ]]; then
+        wait_for_wrapper
+        RESULT=$?
+    else
+        wait_for
+        RESULT=$?
+    fi
+fi
+
+if [[ $CLI != "" ]]; then
+    if [[ $RESULT -ne 0 && $STRICT -eq 1 ]]; then
+        echoerr "$cmdname: strict mode, refusing to execute subprocess"
+        exit $RESULT
+    fi
+    exec $CLI
+else
+    exit $RESULT
+fi
